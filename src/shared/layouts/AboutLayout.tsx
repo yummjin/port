d9eaa99d1711ef17e99b920f014e5e067862e31d
cho4u4o <cho4u4o@gmail.com>
2025-08-20 19:38:02 +0900

    feat : add metadata

--- src/shared/layouts/AboutLayout.tsx
+++ src/shared/layouts/AboutLayout.tsx
@@ -1,16 +1,22 @@
 import { geistMono, geistSans, gothicA1 } from "@/styles/font";
 import AboutHeader from "@/widgets/menu/AboutHeader";
+import Head from "next/head";
 import type { ReactNode } from "react";
 
 export default function AboutLayout({ children }: { children: ReactNode }) {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} ${gothicA1.variable} min-w-screen min-h-screen p-5 md:p-10`}
-    >
-      <AboutHeader />
-      <div className="pt-5 md:pt-10 font-sans flex flex-col gap-2.5 md:gap-5">
-        {children}
+    <>
+      <Head>
+        <title>한유진</title>
+      </Head>
+      <div
+        className={`${geistSans.variable} ${geistMono.variable} ${gothicA1.variable} min-w-screen min-h-screen p-5 md:p-10`}
+      >
+        <AboutHeader />
+        <div className="pt-5 md:pt-10 font-sans flex flex-col gap-2.5 md:gap-5">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
