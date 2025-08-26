e9932275086dd13bb3220eb2b599a0e0fc50af94
cho4u4o <cho4u4o@gmail.com>
2025-08-27 01:42:28 +0900

    design : change padding

--- src/shared/layouts/AboutLayout.tsx
+++ src/shared/layouts/AboutLayout.tsx
@@ -13,9 +13,7 @@ export default function AboutLayout({ children }: { children: ReactNode }) {
         className={`${geistSans.variable} ${geistMono.variable} ${gothicA1.variable} min-w-screen min-h-screen p-5 md:p-10 md:pt-5`}
       >
         <AboutHeader />
-        <div className="pt-5 md:pt-10 font-sans flex flex-col gap-4">
-          {children}
-        </div>
+        <div className="pt-10 font-sans flex flex-col gap-4">{children}</div>
       </div>
     </>
   );
