f1ba82b6a51c25bd4a185f8f21ecfdf97a0a928b
cho4u4o <cho4u4o@gmail.com>
2025-10-29 19:43:44 +0900

    feat : integrate Vercel Analytics for improved tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { geistMono, wantedSans } from "@/styles/font";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <div className={`${geistMono.variable} ${wantedSans.variable}`}>
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </ThemeProvider>
   );
