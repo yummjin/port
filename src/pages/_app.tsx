091ea8c854d0841ac6d536db6e27f11aaf052f5a
cho4u4o <cho4u4o@gmail.com>
2025-12-08 01:00:23 +0900

    remove : remove theme and unused components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,13 @@ import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import type { AppProps } from "next/app";
 
-import { ThemeProvider } from "@/contexts/ThemeContext";
 import { geistMono, wantedSans } from "@/styles/font";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <div className={`${geistMono.variable} ${wantedSans.variable}`}>
-        <Component {...pageProps} />
-        <Analytics />
-      </div>
-    </ThemeProvider>
+    <div className={`${geistMono.variable} ${wantedSans.variable}`}>
+      <Component {...pageProps} />
+      <Analytics />
+    </div>
   );
 }
