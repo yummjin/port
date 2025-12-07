e3325f46889d1550f95f2a62b7680ec572ad0d4f
yummjin <cho4u4o@gmail.com>
2025-12-07 18:35:00 +0900

    chore : set eslint import and prettier, auto fix on save option on vscode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
   {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+      import: importPlugin,
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "warn",
@@ -22,17 +40,41 @@ const eslintConfig = [
         },
       ],
       "no-unused-vars": "off",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", ["parent", "sibling"], "index"],
+          pathGroups: [
+            {
+              pattern: "@/app/**",
+              group: "external",
+              position: "after",
+            },
+            {
+              pattern: "@/page/**",
+              group: "external",
+              position: "after",
+            },
+            {
+              pattern: "@/widgets/**",
+              group: "external",
+              position: "after",
+            },
+            {
+              pattern: "@/shared/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+        },
+      ],
     },
-  },
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
   },
 ];
 
-export default eslintConfig;
+export default config;
