86d81052ab070aa021de56957de1171f387bb156
cho4u4o <cho4u4o@gmail.com>
2025-12-10 17:19:24 +0900

    design : adjust component designs

--- src/shared/components/CardWrapper.tsx
+++ src/shared/components/CardWrapper.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export default function CardWrapper({ children }: { children: ReactNode }) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
       {children}
     </div>
   );
