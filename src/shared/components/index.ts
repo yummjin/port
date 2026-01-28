c4b53b5797182797350e36c076fffabbe1d524b3
yummjin <cho4u4o@gmail.com>
2026-01-28 19:25:19 +0900

    remove : remove unused sidebar component

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Card } from "./Card";
 export { default as CardWrapper } from "./CardWrapper";
 export { default as Title } from "./Title";
-export { default as Sidebar } from "./Sidebar/Sidebar";
